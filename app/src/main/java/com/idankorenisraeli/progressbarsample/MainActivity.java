4de34816cfc97cec0ed5d98759fffc7eb2ee974d
Idan Koren-Israeli <idan503@github.com>
2020-11-15 17:28:07 +0200

    Change bar value methods

--- app/src/main/java/com/idankorenisraeli/progressbarsample/MainActivity.java
+++ app/src/main/java/com/idankorenisraeli/progressbarsample/MainActivity.java
@@ -3,15 +3,47 @@ package com.idankorenisraeli.progressbarsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.idankorenisraeli.customprogressbar.CustomProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustomProgressBar customProgressBar;
+    Button increaseBtn, decreaseBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+
+
+        increaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customProgressBar.increase(0.1f);
+                customProgressBar.invalidate();
+            }
+        });
+
+        decreaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customProgressBar.decrease(0.1f);
+            }
+        });
+
+    }
+
+
+    private void findViews(){
+        customProgressBar = findViewById(R.id.main_CPB_example_bar);
+        increaseBtn = findViewById(R.id.main_BTN_increase);
+        decreaseBtn = findViewById(R.id.main_BTN_decrease);
     }
 }
--- customprogressbar/src/main/java/com/idankorenisraeli/customprogressbar/CustomProgressBar.java
+++ customprogressbar/src/main/java/com/idankorenisraeli/customprogressbar/CustomProgressBar.java
@@ -1,5 +1,6 @@
 package com.idankorenisraeli.customprogressbar;
 
+import android.animation.LayoutTransition;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -15,6 +16,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class CustomProgressBar extends FrameLayout {
     //region Inner Views
@@ -44,8 +48,14 @@ public class CustomProgressBar extends FrameLayout {
     private TextType textType; // Should the text show a custom string, or show current bar percentage/decimal value
     private String textTitle;
     private int textPadding;
+    private int textPaddingLeft;
+    private int textPaddingStart;
+    private int textPaddingRight;
+    private int textPaddingEnd;
 
     //endregion
+
+    private final static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
 
 
 
@@ -97,7 +107,12 @@ public class CustomProgressBar extends FrameLayout {
             textTitle = array.getString(R.styleable.CustomProgressBar_textTitle);
             textEnabled = array.getBoolean(R.styleable.CustomProgressBar_textEnabled, true);
             textColor = array.getColor(R.styleable.CustomProgressBar_textColor, 0);
-            textPadding = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPadding, 3);
+
+            textPadding = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPadding, 0);
+            textPaddingStart = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPaddingStart, 0);
+            textPaddingEnd = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPaddingEnd, 0);
+            textPaddingLeft = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPaddingLeft, 0);
+            textPaddingRight = array.getDimensionPixelSize(R.styleable.CustomProgressBar_textPaddingRight, 0);
         } finally {
             array.recycle();
         }
@@ -134,26 +149,30 @@ public class CustomProgressBar extends FrameLayout {
         FrameLayout.LayoutParams holderParams = new FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         holderParams.setMargins(barPadding,barPadding,barPadding,barPadding);
+
         foregroundHolder.setLayoutParams(holderParams);
         foregroundHolder.setOrientation(LinearLayout.HORIZONTAL);
         foregroundHolder.setTranslationZ(90); //bring to front
         foregroundHolder.setWeightSum(1);
 
+        LayoutTransition lt = new LayoutTransition();
+        lt.disableTransitionType(LayoutTransition.DISAPPEARING);
+        foregroundHolder.setLayoutTransition(lt);
+
         
 
         foregroundCard = new CardView(context);
 
-        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
-        cardParams.weight = value;
-        foregroundCard.setLayoutParams(cardParams);
 
-
+        updateValue();
 
         foregroundCard.setCardBackgroundColor(colorStart); // foreground colors from attr, can be changed by percent
         foregroundCard.setRadius(cornerRadius); //from attrs
 
         foregroundHolder.addView(foregroundCard);
     }
+
+
 
 
     /**
@@ -170,19 +189,32 @@ public class CustomProgressBar extends FrameLayout {
         text = new TextView(context);
         FrameLayout.LayoutParams params =
                 new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        params.setMargins(textPadding,textPadding,textPadding,textPadding);
+
+        if(textPadding!=0)
+            params.setMargins(textPadding, textPadding, textPadding, textPadding);
+
+
+        if(textPaddingStart!=0 || textPaddingEnd!=0) {
+            params.setMarginStart(textPaddingStart);
+            params.setMarginEnd(textPaddingEnd);
+        }
+        else {
+            if(textPaddingLeft !=0 || textPaddingEnd != 0)
+                params.setMargins(textPaddingLeft, textPadding, textPaddingRight, textPadding);
+        }
+
         text.setLayoutParams(params);
 
+        text.setMaxLines(1);
+        text.setLines(1);
         text.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-
-
 
         switch (textGravity){
             case START:
                 text.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
                 break;
             case END:
-                text.setGravity(Gravity.END  | Gravity.CENTER_VERTICAL);
+                text.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
                 break;
             case LEFT:
                 text.setGravity(Gravity.LEFT  | Gravity.CENTER_VERTICAL);
@@ -193,15 +225,51 @@ public class CustomProgressBar extends FrameLayout {
                 //tbd
                 break;
 
-
             default:
                 text.setGravity(Gravity.CENTER  | Gravity.CENTER_VERTICAL);
         }
 
 
+        updateText();
+
+
+        text.setTextColor(textColor);
+        text.setTranslationZ(90);
+
+    }
+
+    @Override
+    public void invalidate() {
+        super.invalidate();
+        updateValue();
+        updateText();
+    }
+
+    /**
+     * Updates the inner bar to fill the layout percentage by current value
+     */
+    private void updateValue()
+    {
+
+        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
+        cardParams.weight = value;
+        foregroundCard.setLayoutParams(cardParams);
+
+    }
+
+    /**
+     * Setting the inner text to show the current value of the bar
+     */
+    private void updateText()
+    {
+        if(!textEnabled)
+            return;
+
         switch (textType){
             case DECIMAL:
-                text.setText(value + "");
+                String decimalString = String.format(getResources().getString(R.string.bar_text_decimal_value), value);
+                text.setText(decimalString);
+
                 break;
             case PERCENTAGE:
                 int percentage = (int)(value * 100);
@@ -211,16 +279,10 @@ public class CustomProgressBar extends FrameLayout {
                 text.setText(textTitle); //setting the custom text given
                 break;
         }
-
-
-        text.setTextColor(textColor);
-        text.setTranslationZ(90);
-
     }
 
 
-
-
+    //region Getters & Setters
     public float getValue() {
         return value;
     }
@@ -315,5 +377,110 @@ public class CustomProgressBar extends FrameLayout {
 
     public void setTextColor(int textColor) {
         this.textColor = textColor;
+    }
+
+    public CardView getBackgroundCard() {
+        return backgroundCard;
+    }
+
+    public void setBackgroundCard(CardView backgroundCard) {
+        this.backgroundCard = backgroundCard;
+    }
+
+    public CardView getForegroundCard() {
+        return foregroundCard;
+    }
+
+    public void setForegroundCard(CardView foregroundCard) {
+        this.foregroundCard = foregroundCard;
+    }
+
+    public LinearLayout getForegroundHolder() {
+        return foregroundHolder;
+    }
+
+    public void setForegroundHolder(LinearLayout foregroundHolder) {
+        this.foregroundHolder = foregroundHolder;
+    }
+
+    public TextView getText() {
+        return text;
+    }
+
+    public void setText(TextView text) {
+        this.text = text;
+    }
+
+    public boolean isTextEnabled() {
+        return textEnabled;
+    }
+
+    public void setTextEnabled(boolean textEnabled) {
+        this.textEnabled = textEnabled;
+    }
+
+    public int getTextPadding() {
+        return textPadding;
+    }
+
+    public void setTextPadding(int textPadding) {
+        this.textPadding = textPadding;
+    }
+
+    public int getTextPaddingLeft() {
+        return textPaddingLeft;
+    }
+
+    public void setTextPaddingLeft(int textPaddingLeft) {
+        this.textPaddingLeft = textPaddingLeft;
+    }
+
+    public int getTextPaddingStart() {
+        return textPaddingStart;
+    }
+
+    public void setTextPaddingStart(int textPaddingStart) {
+        this.textPaddingStart = textPaddingStart;
+    }
+
+    public int getTextPaddingRight() {
+        return textPaddingRight;
+    }
+
+    public void setTextPaddingRight(int textPaddingRight) {
+        this.textPaddingRight = textPaddingRight;
+    }
+
+    public int getTextPaddingEnd() {
+        return textPaddingEnd;
+    }
+
+    public void setTextPaddingEnd(int textPaddingEnd) {
+        this.textPaddingEnd = textPaddingEnd;
+    }
+
+    //endregion
+
+
+    /**
+     * Increase the current bar value, which is between 0 and 1
+     * @param by - A decimal number that would be added to the bar value
+     */
+    public void increase(float by){
+        value += by;
+        value = Math.min(1,value); // Bar value cannot be > 1
+        value = Float.parseFloat(DECIMAL_FORMAT.format(value));
+        invalidate();
+    }
+
+    /**
+     * Decrease the current bar value, which is between 0 and 1
+     * @param by - A decimal number that would be subtracted from the bar value
+     */
+    public void decrease(float by){
+        value -= by;
+        value = Math.max(0,value); // Bar value cannot be < 0
+        value = Float.parseFloat(DECIMAL_FORMAT.format(value));
+        invalidate();
     }
 }
